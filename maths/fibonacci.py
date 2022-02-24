2d7173dc2c567656774afe84d2729fd5157d4ec9
kallyas <kallyasmedia@gmail.com>
2022-02-24 13:05:58 +0300

    fix: fibonacci

--- maths/fibonacci.py
+++ maths/fibonacci.py
@@ -1,7 +1,6 @@
 """
 This program finds fibonacci numbers.
 """
-import time
 
 def fibonacci(number: int) -> int:
     if not isinstance(number, int):
@@ -13,9 +12,3 @@ def fibonacci(number: int) -> int:
     if number == 1:
         return 1
     return fibonacci(number-1) + fibonacci(number-2)
-
-
-# calculate execution time
-start_time = time.time()
-print(fibonacci(100))
-print("--- %s seconds ---" % (time.time() - start_time))
