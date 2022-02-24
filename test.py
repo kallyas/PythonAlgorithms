ea74d04b6d02146e13ac47604c7b11b98c662c15
kallyas <kallyasmedia@gmail.com>
2022-02-24 12:14:54 +0300

    ch: add unit tests

--- test.py
+++ test.py
@@ -1,35 +1,92 @@
-# from conversions import binary_to_decimal as b2d, decimal_to_binary as d2b
+import unittest
 
-from math import factorial
-from ciphers.ceaser_cipher import decrypt, encrypt
-from ciphers.morse_code import morse_decode, morse_encode
-from conversions.binary_to_decimal import binary_to_decimal
-from conversions.decimal_to_binary import decimal_to_binary
-from conversions.decimal_to_octal import decimal_to_octal
-from conversions.hexa_decimal_to_decimal import hexa_decimal_to_decimal
-from conversions.decimal_to_hexa import decimal_to_hexa
-from conversions.octal_to_decimal import octal_to_decimal
-from maths.absolute_max import absolute_max
-from maths.absolute_min import absolute_min
-from maths.check_prime import is_prime
-from maths.fast_exponential import fast_exponential
-from maths.perfect_square import is_perfect_square
+from conversion import *
 
 
 
-print(decimal_to_binary("708"))
-print(binary_to_decimal("1011000100"))
-print(hexa_decimal_to_decimal("1A"))
-print(decimal_to_hexa(26))
-print(octal_to_decimal("17"))
-print(decimal_to_octal("15"))
-print(absolute_max([-1, -2, -3, -4, -5]))
-print(absolute_min(["-1", "-2", "-3", "-4", "-5"]))
-print(is_prime(7))
-print(factorial(5))
-print(fast_exponential(2, 5))
-print(is_perfect_square(16))
-print(encrypt("hello", 3))
-print(decrypt("khoor", 3))
-# print(morse_encode("hello".upper()))
-# print(morse_decode("......-...-..---"))
+class TestConversions(unittest.TestCase):
+
+
+    def test_decimal_to_binary(self):
+        self.assertEqual(decimal_to_binary('8'), '1000')
+        self.assertEqual(decimal_to_binary('10'), '1010')
+        self.assertEqual(decimal_to_binary('15'), '1111')
+
+    def test_decimal_to_binary_invalid(self):
+        self.assertRaises(ValueError, decimal_to_binary, 'abc')
+        self.assertRaises(ValueError, decimal_to_binary, '1.5')
+        self.assertRaises(ValueError, decimal_to_binary, '-1')
+
+    def test_decimal_to_binary_empty(self):
+        self.assertRaises(ValueError, decimal_to_binary, '')
+
+    def test_binary_to_decimal(self):
+        self.assertEqual(binary_to_decimal('1000'), 8)
+        self.assertEqual(binary_to_decimal('1010'), 10)
+        self.assertEqual(binary_to_decimal('1111'), 15)
+
+    def test_binary_to_decimal_invalid(self):
+        self.assertRaises(ValueError, binary_to_decimal, 'abc')
+        self.assertRaises(ValueError, binary_to_decimal, '1.5')
+        self.assertRaises(ValueError, binary_to_decimal, '-1')
+
+    def test_binary_to_decimal_empty(self):
+        self.assertRaises(ValueError, binary_to_decimal, '')
+
+    def test_hexa_decimal_to_decimal(self):
+        self.assertEqual(hexa_decimal_to_decimal('A'), 10)
+        self.assertEqual(hexa_decimal_to_decimal('F'), 15)
+        self.assertEqual(hexa_decimal_to_decimal('1'), 1)
+
+    def test_hexa_decimal_to_decimal_invalid(self):
+        self.assertRaises(ValueError, hexa_decimal_to_decimal, 'abc')
+        self.assertRaises(ValueError, hexa_decimal_to_decimal, '1.5')
+        self.assertRaises(ValueError, hexa_decimal_to_decimal, '-1')
+
+    def test_hexa_decimal_to_decimal_empty(self):
+        self.assertRaises(ValueError, hexa_decimal_to_decimal, '')
+
+    def test_decimal_to_hexa(self):
+        self.assertEqual(decimal_to_hexa(10), 'A')
+        self.assertEqual(decimal_to_hexa(15), 'F')
+        self.assertEqual(decimal_to_hexa(1), '1')
+
+    def test_decimal_to_hexa_invalid(self):
+        self.assertRaises(ValueError, decimal_to_hexa, 'abc')
+        self.assertRaises(ValueError, decimal_to_hexa, '1.5')
+        self.assertRaises(ValueError, decimal_to_hexa, '-1')
+
+    def test_decimal_to_hexa_empty(self):
+        self.assertRaises(ValueError, decimal_to_hexa, '')
+
+    def test_octal_to_decimal(self):
+        self.assertEqual(octal_to_decimal('8'), 8)
+        self.assertEqual(octal_to_decimal('10'), 10)
+        self.assertEqual(octal_to_decimal('15'), 15)
+
+    def test_octal_to_decimal_invalid(self):
+        self.assertRaises(ValueError, octal_to_decimal, 'abc')
+        self.assertRaises(ValueError, octal_to_decimal, '1.5')
+        self.assertRaises(ValueError, octal_to_decimal, '-1')
+
+    def test_octal_to_decimal_empty(self):
+        self.assertRaises(ValueError, octal_to_decimal, '')
+
+    def test_decimal_to_octal(self):
+        self.assertEqual(decimal_to_octal(8), '8')
+        self.assertEqual(decimal_to_octal(10), '10')
+        self.assertEqual(decimal_to_octal(15), '15')
+
+    def test_decimal_to_octal_invalid(self):
+        self.assertRaises(ValueError, decimal_to_octal, 'abc')
+        self.assertRaises(ValueError, decimal_to_octal, '1.5')
+        self.assertRaises(ValueError, decimal_to_octal, '-1')
+
+    def test_decimal_to_octal_empty(self):
+        self.assertRaises(ValueError, decimal_to_octal, '')
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
